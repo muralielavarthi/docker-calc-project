05781a6c824be0d6c9bca7c0ceb70fb929a790e8
Murali Elavarthi <muralis2p@gmail.com>
2026-03-17 19:33:17 +0530

    c4

--- App-server/code/index.js
+++ App-server/code/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.end("Hello from backend container!");
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
